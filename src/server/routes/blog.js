e1ca6e9b26b186e97fc6b881e779290e6b9bc895
MarvinAmari <contact@marvinamari.com>
2020-12-16 19:43:37 -0500

    seperate controllers into directory

--- src/server/routes/blog.js
+++ src/server/routes/blog.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
 // No we can handle any incoming routes
+const { time } = require('../controllers/blog')
 
-router.get('/', (req, res) => {
-  res.json({time: Date().toString()});
-})
+router.get('/', time);
 
 // No we export the routes for use in server.js
 module.exports = router
